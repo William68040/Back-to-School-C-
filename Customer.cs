a8f59f97b2ace9aa0ae026962ba9240a8af51321
William68040 <williamluc888@outlook.com>
2026-02-13 14:19:52 +0100

    skapade en klass som heter Customer med properties för namn, efternamn, telefonnummer och email (fråga 8)

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_to_School_C_
+{
+    internal class Customer
+    {
+        //properties för en customer
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}

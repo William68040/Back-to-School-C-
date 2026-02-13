3fc40a76f3cd573c4a5af5ae102561ddfd4697ea
William68040 <williamluc888@outlook.com>
2026-02-13 11:36:35 +0100

    Skapade fråga 1 med en enkel IF sats

--- WilliamLucQuizz.cs
+++ WilliamLucQuizz.cs
@@ -8,5 +8,20 @@ namespace Back_to_School_C_
 {
     internal class WilliamLucQuizz
     {
+        public static void Fråga1()
+        {
+            // Deklarera och instansiera en strängvariabel med användarens input.
+            // Om värdet är "y" skriv ut "Du svarade ja". 
+
+            string svar = Console.ReadLine();
+            if (svar == "y")
+            {
+                Console.WriteLine("Du svarade ja");
+            }
+            else
+            {
+                Console.WriteLine($"Du svarade {svar}");
+            }
+        }
     }
 }
